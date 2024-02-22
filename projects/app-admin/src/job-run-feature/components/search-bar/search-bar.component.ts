756a6c1d85e11e8acaf33e2a2ad7e266a0479543
Sandeep Deb <deb.sandeep@gmail.com>
2024-02-23 01:21:18 +0530

    Date range working

--- projects/app-admin/src/job-run-feature/components/search-bar/search-bar.component.ts
+++ projects/app-admin/src/job-run-feature/components/search-bar/search-bar.component.ts
@@ -3,6 +3,11 @@ import {DatePipe, NgForOf, NgIf, SlicePipe} from "@angular/common";
 import {PipesModule} from "../../pipes/pipes.module";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {JobRunStatusModel} from "../../model/job-run-status.model";
+import "jquery" ;
+import {from} from "rxjs";
+import {SearchCriteria} from "../../model/job-run-status.vo";
+
+declare var $:any ;
 
 @Component({
   selector: 'search-bar',
@@ -24,4 +29,29 @@ export class SearchBarComponent {
   multiSelect:boolean = false ;
 
   constructor( public model : JobRunStatusModel ) {}
+
+  ngAfterViewInit() {
+    $(".datepicker").datepicker({
+      format: 'dd/mm/yy',
+      autoclose: true,
+      calendarWeeks: true,
+      todayHighlight: true
+    });
+  }
+
+  clearSearchCriteria() : void {
+    let sc: SearchCriteria = this.model.getSearchCriteria() ;
+    sc.reset() ;
+    this.search() ;
+  }
+
+  search() : void {
+    let sc: SearchCriteria = this.model.getSearchCriteria() ;
+
+    sc.fromDate = $('#fromDate').datepicker( 'getDate' ) ;
+    sc.toDate   = $('#toDate').datepicker( 'getDate' ) ;
+    sc.pageNum  = 0 ;
+
+    this.model.fetchSearchResultsFromServer() ;
+  }
 }
--- projects/app-admin/src/job-run-feature/model/job-run-status.vo.ts
+++ projects/app-admin/src/job-run-feature/model/job-run-status.vo.ts
@@ -1,12 +1,21 @@
 export class SearchCriteria {
   constructor(
-    public jobName:string[]|null=[],
+    public jobName:string[]=[],
     public result:string|null=null,
     public fromDate:Date|null=null,
     public toDate:Date|null=null,
     public pageNum:number=0,
     public pageSize:number=37
   ) {}
+
+  reset() {
+    this.jobName = [] ;
+    this.result = null ;
+    this.fromDate = null ;
+    this.toDate = null ;
+    this.pageNum = 0 ;
+    this.pageSize = 37 ;
+  }
 }
 
 export interface JobRunStatus {
